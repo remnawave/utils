96165c3b318523b5099de520796ae97b29c7c630
kastov <kastov@gog.sh>
2025-12-30 20:33:40 +0300

    feat: implement RootLayout component with ScrollToTopWrapper

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -2,24 +2,34 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
     Navigate,
+    Outlet,
     Route,
     RouterProvider
 } from 'react-router-dom'
 
+import { ScrollToTopWrapper } from '@shared/ui/scroll-to-top/scroll-to-top-wrapper'
 import { CryptohappPlaygroundPage } from '@pages/cryptohapp-playground'
 import { HappRoutingBuilderPage } from '@pages/happ-routing-builder'
 import { HomePage } from '@pages/home'
 
 import { ROUTES } from '../../shared/constants'
 
+function RootLayout() {
+    return (
+        <ScrollToTopWrapper>
+            <Outlet />
+        </ScrollToTopWrapper>
+    )
+}
+
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <>
-            <Route element={<HomePage />} path={ROUTES.ROOT} />
+        <Route element={<RootLayout />} path="/">
+            <Route element={<HomePage />} index />
             <Route element={<CryptohappPlaygroundPage />} path={ROUTES.CRYPTOHAPP_PLAYGROUND} />
             <Route element={<HappRoutingBuilderPage />} path={ROUTES.HAPP_ROUTING_BUILDER} />
             <Route element={<Navigate replace to={ROUTES.ROOT} />} path="*" />
-        </>
+        </Route>
     )
 )
 
